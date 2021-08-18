7cff0b976d0be65d7ddf9937ed513863e61b434a
Alex Stearn <alex.wmstearn@gmail.com>
2021-08-18 16:38:02 +0200

    starting with airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all
+
+Flat.create!(
+  [
+    {
+      name: 'Light & Spacious Garden Flat London',
+      address: '10 Clifton Gardens London W9 1DT',
+      description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+      price_per_night: 75,
+      number_of_guests: 3
+  },
+  {
+      name: 'Large Room & Sunny Garden in Berlin Mitte',
+      address: '15 Weinbergsweg, Mitte, Berlin, 10079',
+      description: 'Perfectly located house with sunny garden in the Historical Center of Berlin. The large and quiet bedrooom based at the 1st floor of our peaceful house, has it\'s own private balcony.',
+      price_per_night: 55,
+      number_of_guests: 5
+  }
+]
+)
